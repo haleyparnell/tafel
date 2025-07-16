4e131c25c88b24728dd36718d79a8cbcd8af9a0f
Haley Parnell <hparnell@unc.edu>
2025-07-15 21:55:34 -0400

    Initialize repository

---
+++ Tafel/Tafel/TafelApp.swift
@@ -0,0 +1,17 @@
+//
+//  TafelApp.swift
+//  Tafel
+//
+//  Created by Haley Parnell on 7/15/25.
+//
+
+import SwiftUI
+
+@main
+struct TafelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
